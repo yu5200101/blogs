ed321057ee595212cb8fc5bce0540a605c511a6a
yuruyuan <1377151258@qq.com>
2025-03-17 21:15:03 +0800

    更新资料

--- crossDomain/cors/cors-server2.js
+++ crossDomain/cors/cors-server2.js
@@ -27,8 +27,11 @@ let express = require('express')
 let app = express()
 let whitList = ['http://localhost:3000'] //设置白名单
 app.use(function (req, res, next) {
+  // 可以设置中文
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
   let origin = req.headers.origin
   if (whitList.includes(origin)) {
+    res.setHeader('serve-name', 'put-serve-name') //返回一个响应头，后台需设置
     // 设置哪个源可以访问我
     res.setHeader('Access-Control-Allow-Origin', origin)
     // 允许携带哪个头访问我
@@ -40,7 +43,7 @@ app.use(function (req, res, next) {
     // 预检的存活时间
     res.setHeader('Access-Control-Max-Age', 6)
     // 允许返回的头
-    res.setHeader('Access-Control-Expose-Headers', 'name')
+    res.setHeader('Access-Control-Expose-Headers', 'serve-name')
     if (req.method === 'OPTIONS') {
       res.end() // OPTIONS请求不做任何处理
     }
@@ -48,13 +51,12 @@ app.use(function (req, res, next) {
   next()
 })
 app.put('/getData', function (req, res) {
-  console.log(req.headers)
-  res.setHeader('name', 'jw') //返回一个响应头，后台需设置
-  res.end('我不爱你')
+  console.log('put')
+  res.send('put请求')
 })
 app.get('/getData', function (req, res) {
-  console.log(req.headers)
-  res.end('我不爱你')
+  console.log('get')
+  res.send('get请求')
 })
 app.use(express.static(__dirname))
 app.listen(4000)
