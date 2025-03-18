26ac3215fa0f4cf37c23afa049e23a3765f9a058
yuruyuan <1377151258@qq.com>
2025-03-18 11:32:35 +0800

    cors

--- crossDomain/cors/cors-server1.js
+++ crossDomain/cors/cors-server1.js
@@ -1,4 +1,4 @@
-//server1.js
+//server1.js 通过http://localhost:3000/browser/index.html访问html
 let express = require('express');
 let app = express();
 app.use(express.static(__dirname));
--- crossDomain/cors/cors-server2.js
+++ crossDomain/cors/cors-server2.js
@@ -5,15 +5,18 @@ jsonp 只能支持 get 请求，cors 可以支持多种请求。cors 并不需�
 只要服务器设置的Access-Control-Allow-Origin Header和请求来源匹配，浏览器就允许跨域
 
 1.请求的方法是get，head或者post。
-2.Content-Type是application/x-www-form-urlencoded, multipart/form-data 或 text/plain中的一个值，或者不设置也可以，一般默认就是application/x-www-form-urlencoded。
-3.请求中没有自定义的HTTP头部，如x-token。(应该是这几种头部 Accept，Accept-Language，Content-Language，Last-Event-ID，Content-Type）
+2.请求中没有自定义的HTTP头部，如x-token。(应该是这几种头部 Accept，Accept-Language，Content-Language，Content-Type）
+3.Content-Type是application/x-www-form-urlencoded, multipart/form-data 或 text/plain中的一个值，或者不设置也可以，一般默认就是application/x-www-form-urlencoded。
 */
 // 简单跨域请求
 /* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'xxx');
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Expose-Headers', 'serve-name')
+  （包括Cache-Control、Content-Language、Content-Type、Expires、Last-Modified和Pragma）
 }) */
 // 带预检（Preflighted）的跨域请求
-// 不满足简单跨域请求的，即是带预检的跨域请求。服务端需要设置Access-Control-Allow-Origin(允许跨域资源请求的域)。Acceess-Control-Allow-Methods(允许的请求方法)和Access-Control-Allow-Headers(允许的请求头)
+// 不满足简单跨域请求的，即是带预检的跨域请求。服务端需要设置Access-Control-Allow-Origin(允许跨域资源请求的域)。Access-Control-Allow-Methods(允许的请求方法)和Access-Control-Allow-Headers(允许的请求头)
 /* app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'XXX');
   res.setHeader('Access-Control-Allow-Headers', 'XXX');// 允许返回的头
@@ -45,6 +48,7 @@ app.use(function (req, res, next) {
     // 允许返回的头
     res.setHeader('Access-Control-Expose-Headers', 'serve-name')
     if (req.method === 'OPTIONS') {
+      console.log('OPTIONS')
       return res.end() // OPTIONS请求不做任何处理
     }
   }
