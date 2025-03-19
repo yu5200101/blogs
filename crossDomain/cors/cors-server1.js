f50e3e08d6fcdca9cbb7cc37431beafb4ab9c826
yuruyuan <1377151258@qq.com>
2025-03-19 15:07:18 +0800

    cors

--- crossDomain/cors/cors-server1.js
+++ crossDomain/cors/cors-server1.js
@@ -1,4 +1,5 @@
-//server1.js 通过http://localhost:3000/browser/index.html访问html
+//server1.js 通过http://localhost:3000/browser/index.html
+// 访问html
 let express = require('express');
 let app = express();
 app.use(express.static(__dirname));
