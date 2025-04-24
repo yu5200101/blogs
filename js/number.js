e96954b17bdc08be6a03d58bf49cbfd14585ff17
yu5200101 <1377151258@qq.com>
2025-04-24 13:09:26 +0800

    feat

--- js/number.js
+++ js/number.js
@@ -7,15 +7,37 @@ function add (num1, num2) {
 
 console.log(add(1.2, 3.5))
 
-const big = require('big.js')
+// const big = require('big.js')
 
-const x = new big('7833234432148321')
-const y = new big('7385135793214325')
+// const x = new big('7833234432148321')
+// const y = new big('7385135793214325')
 
-const result = x.plus(y)
-console.log(result.toString())
+// const result = x.plus(y)
+// console.log(result.toString())
 
 const bigNum1 = 7833234432148321n
 const bigNum2 = 7385135793214325n
 
 console.log(bigNum1 + bigNum2)
+
+// 大数相加
+function addBigNumbers(num1, num2) {
+  let curry = 0
+  let num1Index = num1.length - 1
+  let num2Index = num2.length - 1
+  const result = []
+  while(num1Index >= 0 || num2Index >= 0 || curry >= 1) {
+    const digit1 = num1Index >= 0 ? parseInt(num1[num1Index], 10) : 0
+    const digit2 = num2Index >= 0 ? parseInt(num2[num2Index], 10) : 0
+    const sum = digit1 + digit2 + curry
+    result.unshift(sum % 10)
+    curry = Math.floor(sum / 10)
+    num1Index--
+    num2Index--
+  }
+  let str = result.join('')
+  str = str.replace(/^0+/, '') || '0'
+  return str === '' ? '0' : str
+}
+
+console.log(addBigNumbers('8394723321432958', '389837184832718078'))
