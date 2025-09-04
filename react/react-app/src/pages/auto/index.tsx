d154d611cc2af0cf5a8ad184ec782601b6da58eb
yu5200101 <1377151258@qq.com>
2025-09-04 20:31:26 +0800

    feat

--- react/react-app/src/pages/auto/index.tsx
+++ react/react-app/src/pages/auto/index.tsx
@@ -1,15 +1,44 @@
-import React, { useDeferredValue, useState } from 'react';
+import React, { useDeferredValue, useState, useRef } from 'react';
 import List from './components/List'
 
 const Default: React.FC = () => {
   const [value, setValue] = useState('')
   const deferredValue = useDeferredValue(value)
+  const [valueDebounce, setValueDebounce] = useState('')
+  const flag = useRef(false)
+  const timer = useRef<ReturnType<typeof setTimeout> | null | number>(null)
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value)
+  }
+
+  function debounce(fn: Function) {
+    return function() {
+      if (timer.current) {
+        clearTimeout(timer.current)
+      }
+      timer.current = setTimeout(() => {
+        fn()
+        clearTimeout(timer.current as number)
+      }, 1000);
+    }
+  }
+  const handleDebounceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (!flag.current) {
+      setValueDebounce(value)
+      flag.current = true
+    }
+    setValueDebounce(value)
+    debounce(() => {
+      console.log(value, 'value')
+    })()
   }
   return <>
     <input value={value} onChange={handleChange}></input>
     <List query={deferredValue} />
+    <span>防抖</span>
+    <input value={valueDebounce} onChange={handleDebounceChange}/>
   </>
 };
 
